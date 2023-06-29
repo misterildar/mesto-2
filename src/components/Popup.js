662de685a65e9803ee23941cf23179ef81eb9640
Ildar <misterildar@gmail.com>
2023-06-29 17:14:54 +0300

    29.06

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -3,16 +3,18 @@ export default class Popup {
   constructor(popup) {
     this._popup = popup;
     this._buttonClose = this._popup.querySelector('.popup__close');
+    this._handleEscClose = this._handleEscClose.bind(this)
+
   }
 
   open() {
     this._popup.classList.add('popup_opened');
-    document.addEventListener('keydown', this._handleEscClose.bind(this));
+    document.addEventListener('keydown', this._handleEscClose);
   }
 
   close() {
     this._popup.classList.remove('popup_opened');
-    document.removeEventListener('keydown', this._handleEscClose.bind(this));
+    document.removeEventListener('keydown', this._handleEscClose);
   }
 
   _handleEscClose(evt) {
